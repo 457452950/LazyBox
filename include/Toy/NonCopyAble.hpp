22e183f2d22da7ef1e156796e88c850359ff623a
wanglb <1602434826@qq.com>
2024-01-21 17:12:20 +0800

    [fix]add namespace.

--- include/Toy/NonCopyAble.hpp
+++ include/Toy/NonCopyAble.hpp
@@ -2,10 +2,14 @@
 #ifndef LAZYBOX_INCLUDE_TOY_NONCOPYABLE_HPP_
 #define LAZYBOX_INCLUDE_TOY_NONCOPYABLE_HPP_
 
+namespace lbox {
+
 class NonCopyAble {
 public:
     NonCopyAble(const NonCopyAble &)            = delete;
     NonCopyAble &operator=(const NonCopyAble &) = delete;
 };
+
+} // namespace lbox
 
 #endif // LAZYBOX_INCLUDE_TOY_NONCOPYABLE_HPP_
