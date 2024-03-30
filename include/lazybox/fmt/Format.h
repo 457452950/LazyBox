48175d42bc7f5167b5c604e7fc92ea8577fc3f99
wanglb <1602434826@qq.com>
2024-03-30 17:23:17 +0800

    [update] use local file without git submodle

--- include/lazybox/fmt/Format.h
+++ include/lazybox/fmt/Format.h
@@ -2,16 +2,16 @@
 #ifndef LAZYBOX_INCLUDE_LAZYBOX_FMT_FORMAT_H_
 #define LAZYBOX_INCLUDE_LAZYBOX_FMT_FORMAT_H_
 
-#include "../../../third_party/fmt/include/fmt/format.h"
-#include "../../../third_party/fmt/include/fmt/color.h"
-#include "../../../third_party/fmt/include/fmt/ranges.h"
-#include "../../../third_party/fmt/include/fmt/chrono.h"
-#include "../../../third_party/fmt/include/fmt/os.h"
-#include "../../../third_party/fmt/include/fmt/std.h"
-#include "../../../third_party/fmt/include/fmt/ostream.h"
-#include "../../../third_party/fmt/include/fmt/compile.h"
-#include "../../../third_party/fmt/include/fmt/core.h"
-#include "../../../third_party/fmt/include/fmt/xchar.h"
+#include <fmt/format.h>
+#include <fmt/color.h>
+#include <fmt/ranges.h>
+#include <fmt/chrono.h>
+#include <fmt/os.h>
+#include <fmt/std.h>
+#include <fmt/ostream.h>
+#include <fmt/compile.h>
+#include <fmt/core.h>
+#include <fmt/xchar.h>
 
 namespace lbox {
 using namespace fmt;
